a309e05349c1c5440f6de4c5ebb191d24cd1958d
M1shko.exe <nezkusil23@gmail.com>
2026-02-27 10:09:02 +0100

    Add files via upload

---
+++ IDE_code_skola.ino
@@ -0,0 +1,73 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(2, 3);  // RX, TX
+
+const int buzzer = 5;
+const int ledstripe = 6;
+const int rst = 4;
+
+bool answer = false;
+bool alarmActive = false;
+unsigned long lastToggle = 0;
+const unsigned long interval = 200; // rychlost blikání
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  pinMode(ledstripe, OUTPUT);
+  pinMode(rst, INPUT);
+  pinMode(8, OUTPUT);
+
+  digitalWrite(buzzer, LOW);
+  digitalWrite(ledstripe, LOW);
+  digitalWrite(8, HIGH);
+  answer = false;
+
+  BT.begin(9600);
+  Serial.begin(9600);
+  BT.println(";)");
+}
+
+void loop() {
+  // Pokud přijde zpráva z HC-05, aktivuje se alarm
+  if (BT.available()) {
+    digitalWrite(8, LOW);
+    char bebra = BT.read();
+    Serial.print("Přijato: ");
+    Serial.println(bebra);
+    if (bebra == '!'){
+      alarmActive = true;
+    }
+  }
+
+  // Pokud je alarm aktivní → blikání + bzučák
+  if (alarmActive) {
+    unsigned long currentMillis = millis();
+    answer = true;
+
+    if (currentMillis - lastToggle >= interval) {
+      lastToggle = currentMillis;
+
+      static bool state = false;
+      state = !state;
+
+      // blikání LED pásku
+      digitalWrite(ledstripe, state);
+
+      // bzučák zap/vyp
+      digitalWrite(buzzer, state);
+    }
+  }
+  else {
+    digitalWrite(ledstripe, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(8, HIGH);
+    answer = false;
+  }
+  if (digitalRead(rst) == HIGH){
+    if (answer == true){
+      BT.println("Přijato");
+      alarmActive = false;
+      answer = false;
+    }
+  }
+}
